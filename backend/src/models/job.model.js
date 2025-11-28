16dc4d559eb72f96c383d238f4e23d46178cd365
anshumancodes <anshumanprof01@gmail.com>
2025-11-28 19:50:35 +0530

    add Job schema and model

---
+++ backend/src/models/job.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const JobModel = mongoose.model("Job", jobSchema);
