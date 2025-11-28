dda04cc41bc342328b60b666af686144b4c4178f
anshumancodes <anshumanprof01@gmail.com>
2025-11-29 00:54:19 +0530

    implement asyncHandler higher-order function to wrap async controllers and handle errors

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (func) => {
+  //  Higher order function->
+
+  return async (req, res, next) => {
+    try {
+      await func(req, res, next);
+      // here am executing the function!
+    } catch (error) {
+      // throws error and false success flag to client side
+      res.status(error.code || 500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+};
+
+export { asyncHandler };
