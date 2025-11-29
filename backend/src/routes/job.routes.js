57cecebec08a97955b617b6e4b483975d93a56b8
anshumancodes <anshumanprof01@gmail.com>
2025-11-30 00:05:52 +0530

    Add route for fetching single job by ID

--- backend/src/routes/job.routes.js
+++ backend/src/routes/job.routes.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
 const router=Router();
-import {createJob,fetchAllJobs,updateJob,deleteJob} from "../controllers/job.controller.js"
+import {createJob,fetchAllJobs,fetchJobById,updateJob,deleteJob} from "../controllers/job.controller.js"
 
 router.route("/fetch").get(fetchAllJobs);
+router.route("/:id").get(fetchJobById);
 router.route("/update/:id").put(updateJob);
 router.route("/delete/:id").delete(deleteJob);
 router.route("/create").post(createJob)
