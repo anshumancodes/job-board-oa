989880be43ca30d2bbb835af4f7ad7a857c4ea46
anshumancodes <anshumanprof01@gmail.com>
2025-11-29 22:25:44 +0530

    add type interfaces for job

---
+++ frontend/src/types/job.ts
@@ -0,0 +1,18 @@
+export interface IJob {
+  _id: string;
+  title: string;
+  company: string;
+  location: string;
+}
+export interface Job {
+  id: string;
+  title: string;
+  company: string;
+  location: string;
+}
+
+export interface IJobForm {
+  title: string;
+  company: string;
+  location: string;
+}
