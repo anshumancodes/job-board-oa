a2e0f91735d9ca396ea13813a539f34030a2c547
anshumancodes <anshumanprof01@gmail.com>
2025-11-29 11:04:36 +0530

    setup Express app with CORS and job routes

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import jobRouter from "./routes/job.routes.js";
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // well this is where my frontend runs so , change it if needed
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json({ limit: "12kb" }));
+app.use(express.urlencoded({ extended: true, limit: "12kb" }));
+
+
+// routes
+app.use("/api/v1/jobs", jobRouter);
+
+export default app;
